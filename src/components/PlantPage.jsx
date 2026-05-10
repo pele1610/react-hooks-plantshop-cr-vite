84d09e3053c99fc82a101031047b6aca7d6074b3
pele1610 <pelecolo56@gmail.com>
2026-05-10 22:45:56 +0300

    Refactor plant management components and enhance form submission functionality

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
+
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+const [searchQuery, setSearchQuery] = useState("");
+  
+  useEffect(() =>{
+    fetch("http://localhost:6001/plants")
+    .then((r) =>r.json())
+    .then((data)=> setPlants(data));
+  },[]);
+
+  function handleAddPlant(newPlant) {
+  setPlants([...plants, newPlant]);
+}
+
+const filteredPlants = plants.filter((plant) =>
+  plant.name.toLowerCase().includes(searchQuery.toLowerCase())
+);
   return (
+    
+
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search searchQuery={searchQuery} onSearch={setSearchQuery} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
